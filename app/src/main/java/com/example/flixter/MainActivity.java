10174955ab52091ad4062460c99cce167b60be65
Selina Kim <selinakim@fb.com>
2020-06-24 14:01:19 -0700

    Gets list of possible image sizes, and added xml file for Recycler view

--- app/src/main/java/com/example/flixter/MainActivity.java
+++ app/src/main/java/com/example/flixter/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
-import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixter.models.Movie;
 
@@ -14,12 +13,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String CONFIGURATION_URL =
+            "https://api.themoviedb.org/3/configuration?api_key=970a3c4389490ee855a71b051c6e0574";
     public static final String NOW_PLAYING_URL =
             "https://api.themoviedb.org/3/movie/now_playing?api_key=970a3c4389490ee855a71b051c6e0574";
     public static final String TAG = "MainActivity";
@@ -43,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.i(TAG, "Results:" + results.toString());
                     movies = Movie.fromJsonArray(results);
                 } catch (JSONException e) {
-                    Log.e(TAG, "Hit json exception", e);
+                    Log.e(TAG, "Hit json exception for movie results", e);
                     e.printStackTrace();
                 }
             }
@@ -53,5 +55,41 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onFailure");
             }
         });
+
+        client.get(CONFIGURATION_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONObject images = jsonObject.getJSONObject("images");
+                    Log.i(TAG, "Images:" + images.toString());
+
+                    JSONArray posterSizes = images.getJSONArray("poster_sizes");
+                    ArrayList<String> posterArr = createStringArrayFromJSON(posterSizes);
+                    Movie.setPoster_sizes(posterArr);
+
+                    JSONArray backdropSizes = images.getJSONArray("backdrop_sizes");
+                    ArrayList<String> backdropArr = createStringArrayFromJSON(backdropSizes);
+                    Movie.setBackdrop_sizes(backdropArr);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception for configuration results", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+    }
+
+    public static ArrayList<String> createStringArrayFromJSON(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> array = new ArrayList<String>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            array.add(jsonArray.getJSONObject(i).toString());
+        }
+        return array;
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -10,18 +10,8 @@ import java.util.List;
 public class Movie {
     private String poster_path;
     private String title;
-
-    public String getPoster_path() {
-        return String.format("",poster_path);
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
+    private static List<String> poster_sizes;
+    private static List<String> backdrop_sizes;
 
     private String overview;
 
@@ -37,5 +27,26 @@ public class Movie {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
+    }
+
+    public static void setPoster_sizes(List<String> poster_sizes) {
+        Movie.poster_sizes = poster_sizes;
+    }
+
+    public static void setBackdrop_sizes(List<String> backdrop_sizes) {
+        Movie.backdrop_sizes = backdrop_sizes;
+    }
+
+    public String getPoster_path() {
+        return "https://image.tmdb.org/t/p/" +
+                poster_sizes.get(poster_sizes.size()-1) + "/" + poster_path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
     }
 }
